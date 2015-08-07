aeab281fd4d2a6c78699323e9aeaf6ed501b8a0e
Adam Won <wonton916@gmail.com>
2015-08-07 15:30:58 -0700

    After lunch changes.

---
+++ app/app.php
@@ -0,0 +1,36 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
+
+    session_start ();
+
+    if (empty ($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get("/", function() use ($app){
+
+        return $app['twig']->render('address_home.html.twig', array('contacts' => Contact::getAll()));
+
+      });
+
+    $app->post("/create_contact", function() use($app){
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
+      });
+
+
+    $app->post("/delete_contacts", function() use ($app){
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contacts.html.twig');
+
+      });
+
+      return $app;
+
+ ?>
